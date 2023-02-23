3c0202184d10f39056ea99e78528012c4935f0b1
HelloYukyung <ygim36204@gmail.com>
2023-02-23 15:47:53 +0000

    fix : header border px 1.5px-> 0.5px

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 function Header() {
-  return <StyledHeader>header</StyledHeader>;
+  return (
+    <StyledHeader>
+      <button>hi</button>
+    </StyledHeader>
+  );
 }
 
 export default Header;
@@ -14,6 +18,7 @@ const StyledHeader = styled.header`
   position: fixed;
   top: 0;
 
+  border-bottom: var(--zero-point-five) solid #c5c7c2;
   z-index: var(--header-z-index);
   /* background-color: var(--primary); */
 `;
