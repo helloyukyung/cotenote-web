76d332d008167b3cbde306511e187860e6308c90
HelloYukyung <ygim36204@gmail.com>
2023-02-25 15:33:45 +0000

    fix : mui console errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  babel: {
+    presets: ["next/babel"],
+    plugins: [
+      ["babel-plugin-styled-components", { ssr: true, displayName: true }],
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
