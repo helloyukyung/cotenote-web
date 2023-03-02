1ba3bf41ac35900188d394e2b4a955c4064544a5
HelloYukyung <ygim36204@gmail.com>
2023-03-02 15:47:16 +0000

    feat : Container Component

---
+++ src/components/common/Container.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div<{ maxWidth?: number }>`
+  max-width: ${(props) => (props.maxWidth ? props.maxWidth : 620)}px;
+
+  box-sizing: content-box;
+  margin: 0 auto;
+  padding: 0 var(--mobile-padding);
+`;
+
+export default Container;
