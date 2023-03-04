64f8a1b367bb168651d5aed20430e5ea7d988754
HelloYukyung <ygim36204@gmail.com>
2023-03-04 17:58:31 +0000

    feat : footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -4,24 +4,26 @@ import styled from "styled-components";
 
 function Footer() {
   return (
-    <StyledFooter>
+    <FooterWrapper>
       <Pen
         src={"/assets/images/footer_pen.svg"}
         width={24}
         height={56}
         alt={"footer_pen"}
       />
-      footer
-    </StyledFooter>
+      <StyledFooter>
+        <p className="footer_text">Copyright © 2022 Cotenote Inc.</p>
+        <p className="footer_text">문의 | cotenoteofficial@gmail.com</p>
+      </StyledFooter>
+    </FooterWrapper>
   );
 }
 
 export default Footer;
 
-const StyledFooter = styled.div`
+const FooterWrapper = styled.div`
   width: 100%;
   height: var(--footer-height);
-
   position: absolute;
   bottom: 0;
   z-index: var(--footer-z-index);
@@ -29,7 +31,33 @@ const StyledFooter = styled.div`
   border-top: var(--one-point-five) solid var(--primary);
 `;
 
+const StyledFooter = styled.footer`
+  max-width: 1296px;
+  padding: 15px 15px 15px;
+
+  display: flex;
+  justify-content: space-between;
+
+  margin: auto;
+
+  .footer_text {
+    font-size: 16px;
+    line-height: 16px;
+    color: var(--text-50);
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.values.tablet}px) {
+    justify-content: right;
+    .footer_text:first-of-type {
+      display: none;
+    }
+    .footer_text {
+      font-size: 14px;
+    }
+  }
+`;
+
 const Pen = styled(Image)`
+  box-sizing: content-box;
   position: absolute;
   top: -56px;
   right: 174px;
