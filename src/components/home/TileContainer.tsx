2281ef8a88a68a9c4256ebcb4ff2b5e0e3bab0a7
HelloYukyung <ygim36204@gmail.com>
2023-04-03 14:44:07 +0000

    fix : tile icon

--- src/components/home/TileContainer.tsx
+++ src/components/home/TileContainer.tsx
@@ -1,14 +1,18 @@
-import { EditIcon, TimeIcon } from "@chakra-ui/icons";
+import { EditIcon, TimeIcon, CheckIcon } from "@chakra-ui/icons";
 import { Grid } from "@chakra-ui/react";
 import React from "react";
+
 import Tile from "./Tile";
 
 function TileContainer() {
   return (
-    <Grid gridTemplateColumns="1fr 1fr 1fr" gap="80px">
-      <Tile title="타이머" icon={<TimeIcon fontWeight="bold" />} />
+    <Grid
+      gridTemplateColumns="1fr 1fr 1fr"
+      gap={["60px", "70px", "75px", "80px"]}
+    >
+      <Tile title="타이머" icon={<TimeIcon />} />
       <Tile title="모의고사" icon={<EditIcon />} />
-      <Tile title="오답노트" icon={<TimeIcon />} />
+      <Tile title="오답노트" icon={<CheckIcon />} />
     </Grid>
   );
 }
